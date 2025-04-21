a02401c785f45407eef3962d1989044a5e0ad133
slapmai <btdwnch@gmail.com>
2025-04-21 13:32:49 +0300

    "Modified extraction script and config file"

--- src/extract.py
+++ src/extract.py
@@ -1,102 +1,76 @@
 import logging
 import requests
 import csv
-import sqlite3
-from main import load_config
 
-# Initialize logging
+# Инициализация логирования
+# Устанавливаем уровень логирования и формат сообщений
 logging.basicConfig(
-    level=logging.INFO, format="%(asctime)s - %(уровень)s - %(message)s"
+    level=logging.INFO, format="%(asctime)s - %(levelname)s - %(message)s"
 )
 
 
+# Будем использовать класс DataExtractor для определения методов извлечения данных4
+# Для демонстрации определим два метода извлечения из API и из CSV
 class DataExtractor:
     def __init__(self, config: dict):
+        # config - словарь с настройками
+        # Он определяется в main.py при инициализации класса
         self.config = config
 
     def extract_from_api(self, endpoint: str, headers: dict = None) -> dict | None:
-        """Data extraction from API.
+        """Метод извлечения данных из API
 
         Args:
-            endpoint (str): api endpoint URL.
-            headers (dict): request headers for authentication.
+            endpoint (str): адрес запроса к API
+            headers (dict, optional): Заголовки запроса к API. По умолчанию None.
 
         Returns:
-            dict: response data in JSON format or None in case of error.
+            dict | None: словарь с данными или None в случае ошибки
         """
+        logging.info(f"Извлечение данных из API: {endpoint}")
 
-        logging.info(f"Data extraction from API: {endpoint}")
+        if not headers:
+            # Если заголовки не указаны, используем headers из конфигурации
+            headers = self.config.get("headers", {})
         try:
-            # Make GET request to the API
-            if headers is None:
-                # Default headers if not provided
-                headers = {}
-                headers["Content-Type"] = "application/json"
-                headers["Accept"] = "application/json"
-                headers["User-Agent"] = (
-                    "Mozilla/5.0 (Windows NT 10.0; Win64; x64) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/58.0.3029.110 Safari/537.3"
-                )
             response = requests.get(endpoint, headers=headers)
-            response.raise_for_status()
-            logging.info("Data successfully extracted from API.")
-            return response.json()
-        except Exception as e:
-            logging.error(f"Error extracting data from API: {e}")
+            response.raise_for_status()  # Проверяем на наличие ошибок
+            try:
+                data = response.json()
+                logging.info("Данные успешно извлечены из API.")
+                return data
+            except ValueError:
+                logging.error("Ответ API не является JSON.")
+                return None
+        except (
+            requests.exceptions.HTTPError,
+            requests.exceptions.ConnectionError,
+            requests.exceptions.RequestException,
+        ) as e:
+            logging.error(f"Ошибка при извлечении данных из API: {e}")
             return None
 
     def extract_from_csv(self, file_path: str) -> list | None:
-        """CSV data extraction.
-        CSV file must have a header row.
+        """Метод извлечения данных из CSV файла
 
         Args:
-            file_path (str): path to the CSV file.
+            file_path (str): путь к файлу CSV
 
         Returns:
-            list: list of dictionaries with data or None in case of error.
+            list | None: список строк из CSV файла или None в случае ошибки
         """
-        logging.info(f"Data extraction from CSV: {file_path}")
+        logging.info(f"Извлечение данных из CSV: {file_path}")
         try:
-            # Read CSV file
-            with open(file_path, mode="r") as file:
-                reader = csv.DictReader(file)
+            with open(file_path, mode="r", encoding="utf-8") as file:
+                reader = csv.reader(file)
                 data = [row for row in reader]
-            logging.info("Data successfully extracted from CSV.")
-            return data
+                logging.info("Данные успешно извлечены из CSV.")
+                return data
+        except FileNotFoundError:
+            logging.error(f"Файл не найден: {file_path}")
+            return None
         except Exception as e:
-            logging.error(f"Error extracting data from CSV: {e}")
+            logging.error(f"Ошибка при извлечении данных из CSV: {e}")
             return None
 
-    def extract_from_sqlite_db(self, connection_string: str, query: str) -> list | None:
-        """Data extraction from SQLite database.
-
-        Args:
-            connection_string (str): SQLite connection string.
-            query (str): SQL query to execute.
-
-        Returns:
-            list: list of tuples with data or None in case of error.
-        """
-        try:
-            logging.info(f"Data extraction from SQLite DB: {connection_string}")
-            # Connect to SQLite database
-            conn = sqlite3.connect(connection_string)
-            cursor = conn.cursor()
-            cursor.execute(query)
-            data = cursor.fetchall()
-            conn.close()
-            logging.info("Данные успешно извлечены из базы данных.")
-            return data
-        except Exception as e:
-            logging.error(f"Ошибка при извлечении данных из базы данных: {e}")
-            return None
-
-    def extract_from_file(self, file_path: str) -> list | None:
-        logging.info(f"Data extraction from file: {file_path}")
-        try:
-            with open(file_path, "r") as file:
-                data = file.readlines()
-            logging.info("Data successfully extracted from file.")
-            return data
-        except Exception as e:
-            logging.error(f"Error extracting data from file: {e}")
-            return None
+    # Add other data extraction methods here
