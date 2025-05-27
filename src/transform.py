163d6e4effc079d747ea8b50eef165474a7da063
slapmai <btdwnch@gmail.com>
2025-05-27 14:55:32 +0300

    fix config import issue

--- src/transform.py
+++ src/transform.py
@@ -30,12 +30,13 @@ class DataTransformer:
             dict: Трансформированные данные в виде DataFrame по таблицам
         """
 
-        cfg = self.config.get("transformations", {})
+        cfg = self.config.get("transformation_config", {})
         transformed = {}
         dropna = cfg.get("dropna", False)
         drop_duplicates = cfg.get("drop_duplicates", False)
 
         for name, payload in data.items():
+            logger.info(f"Трансформация таблицы '{name}'...")
             cols = payload.get("columns", [])
             rows = payload.get("rows", [])
 
@@ -44,11 +45,15 @@ class DataTransformer:
 
             # Удаляем null-значения
             if dropna:
+                logger.info(f"Удаление null-значений в таблице '{name}'")
                 df = df.dropna()
+                logger.info(f"Удалено {len(rows) - len(df)} null-значений в таблице {name}")
 
             # Удаляем дубликаты
             if drop_duplicates:
+                logger.info(f"Удаляем дубликаты в таблице {name}")
                 df = df.drop_duplicates()
+                logger.info(f"Удалено {len(rows) - len(df)} дубликатов в таблице {name}")
 
             # Сохраняем результат
             transformed[name] = df
